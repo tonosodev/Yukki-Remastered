60176378a697dd65f5d3e3ba8e31e4c53bd5b16e
bazyukartyom@gmail.com <Qwsxza2qwsxzA123132>
2020-12-27 22:14:09 +0600

    New help.py embed (need to changes)

--- cogs/phrases/help.py
+++ cogs/phrases/help.py
@@ -11,19 +11,38 @@ class HelpCommandCog(commands.Cog):
 
     @commands.command(aliases=help_command_aliases)
     @commands.has_any_role(*commands_permission['help_command_permission'])
+    @commands.cooldown(1, 30, commands.BucketType.user)
     async def help(self, ctx):
+        await ctx.message.delete()
         embed1 = discord.Embed(title=f"Приветствуем Вас в {ctx.guild.name}",
                                color=0x6A5ACD,
-                               description=f'{ctx.guild.name} - это жилище для людей с чувством юмора и хорошим настроением.'
-                                           f'\n')
+                               description=f'{ctx.guild.name} - жилище для людей с чувством юмора и хорошим настроением.'
+                                           f'\nСпасибо, что Вы с нами!')
         embed1.set_thumbnail(url=ctx.guild.icon_url)
-        embed1.add_field(name='__**Информация**__:', value=f'value', inline=False)
+        embed1.add_field(name='__**Информация**__:',
+                         value=f'Важную к ознакомлению информацию всегда можно найти в следующих каналах:'
+                               f'\n**Локальная информация**:'
+                               f'\n{self.bot.get_channel(766217771151261697).mention}'
+                               f'\n{self.bot.get_channel(766217874422759434).mention}'
+                               f'\n{self.bot.get_channel(768556053682978816).mention}'
+                               f'\n**Серверные оповещения**:'
+                               f'\n{self.bot.get_channel(766213910595633155).mention}'
+                               f'\n{self.bot.get_channel(766213910595633156).mention}'
+                               f'\n**Советуемое к просмотру**:'
+                               f'\n{self.bot.get_channel(774065006186463283).mention}'
+                               f'\n**Спонсоры**:'
+                               f'\n{self.bot.get_channel(766217471514247169).mention}',
+                         inline=False)
         embed1.add_field(name='__**Команды**__:', value='см. раздел II', inline=False)
+        embed1.add_field(name='__**Техническая поддержка**__:', value='см. раздел III', inline=False)
+        embed1.add_field(name='__**Совет**__:', value='Для перемещения по разделам кликайте на эмодзи под этим меню',
+                         inline=False)
+        embed1.add_field(name=f'Информацию запросил:', value=f'{ctx.author.mention}', inline=False)
         embed1.set_footer(text=f'{self.bot.user.name} © 2020 | Все права защищены',
                           icon_url=self.bot.user.avatar_url)
 
         embed2 = discord.Embed(title=f"Команды сервера {ctx.guild.name}",
-                               description=f'Список основных команд сервера {ctx.guild.name}, доступных большинству пользователей.')
+                               description=f'Список основных команд сервера {ctx.guild.name}, доступных пользователям.')
         embed2.add_field(name='__**header**__', value='value')
         embed2.set_footer(text=f'{self.bot.user.name} © 2020 | Все права защищены',
                           icon_url=self.bot.user.avatar_url)
@@ -35,9 +54,9 @@ class HelpCommandCog(commands.Cog):
                           icon_url=self.bot.user.avatar_url)
 
         embeds = [embed1, embed2, embed3]
-        message = await ctx.reply(embed=embed1)
+        message = await ctx.send(embed=embed1)
         page = Paginator(self.bot, message, only=ctx.author, use_more=False, embeds=embeds, language="ru",
-                         footer_icon=self.bot.user.avatar_url, timeout=30, use_exit=False, delete_message=True,
+                         footer_icon=self.bot.user.avatar_url, timeout=60, use_exit=False, delete_message=False,
                          color=0x6A5ACD, use_remove_reaction=True)
         await page.start()
 
