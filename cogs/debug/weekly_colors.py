e8e0c7572f4a04e7a3e3ba7a311377fc1e906a64
Alexey Shvetsov <DevilDesigner@users.noreply.github.com>
2021-03-10 01:34:59 +0600

    New sync system with GitHub

--- cogs/debug/weekly_colors.py
+++ cogs/debug/weekly_colors.py
@@ -4,3 +4,17 @@ Session by: https://github.com/DevilDesigner
 Create Time: 6:12 PM
 This Class: weekly_rolors
 """
+
+import discord
+from discord.ext import commands
+
+from config import commands_permission
+
+
+class WeeklyColorsEmbed(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(WeeklyColorsEmbed(bot))
